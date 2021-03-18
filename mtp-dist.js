daf8d2a453d7fdbaf5427ebbfc1e70008570cff3
juskteez <vhuyphong@gmail.com>
2021-03-18 14:24:50 +0700

    Update product page

--- mtp-dist.js
+++ mtp-dist.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var main_content = document.querySelector('.default-layout');
 var drop_navs = document.querySelectorAll('.site-nav--has-dropdown');
 var header = document.querySelector('.header-section');
 
@@ -21,58 +22,75 @@ for (var i = 0; i < drop_navs.length; i++) {
   });
 }
 
-var vue_slides = document.querySelectorAll('.VueCarousel-slide .slideshow-section__background img');
-
 var init_slides = function init_slides() {
-  var init_slides = document.querySelectorAll('.VueCarousel-slide .slideshow-section__background img');
-  for (var _i = 0; _i < vue_slides.length; _i++) {
+  var vue_slides = document.querySelectorAll('.section[data-id="homepage_slideshow"] .VueCarousel-slide .slideshow-section__background img');
+  var first_slide = document.querySelector('.section[data-id="homepage_slideshow"] .VueCarousel-inner .VueCarousel-slide:first-child');
+  if (!first_slide.classList.contains('VueCarousel-slide-active')) {
+    first_slide.classList.add('VueCarousel-slide-active');
+  }
+
+  var _loop = function _loop(_i) {
     var the_image = vue_slides[_i];
     var image_wrap = the_image.parentElement;
+    var defer_slides = void 0;
 
-    var new_image_1 = the_image.cloneNode(true);
-    var new_image_2 = the_image.cloneNode(true);
+    defer_slides = setInterval(function () {
+      if (the_image.getAttribute('lazy') == 'loaded') {
+        var new_image_1 = the_image.cloneNode(true);
+        var new_image_2 = the_image.cloneNode(true);
 
-    new_image_1.classList.add('sub_1');
-    new_image_2.classList.add('sub_2');
+        var new_wrap_0 = document.createElement('DIV');
+        var new_wrap_1 = document.createElement('DIV');
+        var new_wrap_2 = document.createElement('DIV');
 
-    image_wrap.appendChild(new_image_1);
-    image_wrap.appendChild(new_image_2);
-    console.log(new_image_1);
+        new_wrap_0.classList.add('sub_wrap-1');
+        new_wrap_1.classList.add('sub_wrap-2');
+        new_wrap_2.classList.add('sub_wrap-3');
+
+        new_wrap_0.appendChild(the_image);
+        new_wrap_1.appendChild(new_image_1);
+        new_wrap_2.appendChild(new_image_2);
+
+        image_wrap.appendChild(new_wrap_0);
+        image_wrap.appendChild(new_wrap_1);
+        image_wrap.appendChild(new_wrap_2);
+
+        console.log('lazy_loaded');
+        clearInterval(defer_slides);
+      }
+    }, 100);
+  };
+
+  for (var _i = 0; _i < vue_slides.length; _i++) {
+    _loop(_i);
   }
 };
 
-var _loop = function _loop(_i2) {
-  var the_image = vue_slides[_i2];
-  var image_wrap = the_image.parentElement;
-  var defer_slides = void 0;
-
-  defer_slides = setInterval(function () {
-    if (the_image.getAttribute('lazy') == 'loaded') {
-      var new_image_1 = the_image.cloneNode(true);
-      var new_image_2 = the_image.cloneNode(true);
-
-      var new_wrap_0 = document.createElement('DIV');
-      var new_wrap_1 = document.createElement('DIV');
-      var new_wrap_2 = document.createElement('DIV');
-
-      new_wrap_0.classList.add('sub_wrap-1');
-      new_wrap_1.classList.add('sub_wrap-2');
-      new_wrap_2.classList.add('sub_wrap-3');
-
-      new_wrap_0.appendChild(the_image);
-      new_wrap_1.appendChild(new_image_1);
-      new_wrap_2.appendChild(new_image_2);
-
-      image_wrap.appendChild(new_wrap_0);
-      image_wrap.appendChild(new_wrap_1);
-      image_wrap.appendChild(new_wrap_2);
-
-      console.log('lazy_loaded');
-      clearInterval(defer_slides);
+var main_content_observer = function main_content_observer(mutations) {
+  console.log('Main content changed');
+  var home_slider = document.querySelector('.section[data-id="homepage_slideshow"]');
+  if (home_slider) {
+    if (!home_slider.classList.contains('mutated')) {
+      home_slider.classList.add('mutated');
+      init_slides();
     }
-  }, 100);
+  }
+  var sticky_bar = document.getElementById('sticky-bar');
+  if (sticky_bar) {
+    if (!main_content.classList.contains('stickies')) main_content.classList.add('stickies');
+  } else {
+    if (main_content.classList.contains('stickies')) main_content.classList.remove('stickies');
+  }
 };
 
-for (var _i2 = 0; _i2 < vue_slides.length; _i2++) {
-  _loop(_i2);
+if (main_content) {
+
+  var main_observer = new MutationObserver(main_content_observer);
+  var options = {
+    childList: true,
+    subtree: true
+  };
+
+  main_observer.observe(main_content, options);
+  // console.log('Observing main content')
 }
--- mtp.js
+++ mtp.js
@@ -1,3 +1,4 @@
+let main_content = document.querySelector('.default-layout')
 let drop_navs = document.querySelectorAll('.site-nav--has-dropdown')
 let header    = document.querySelector('.header-section')
 
@@ -19,54 +20,71 @@ for (let i = 0; i < drop_navs.length; i++) {
   })
 }
 
-let vue_slides = document.querySelectorAll('.VueCarousel-slide .slideshow-section__background img')
-
 const init_slides = () => {
-  let init_slides = document.querySelectorAll('.VueCarousel-slide .slideshow-section__background img')
+  let vue_slides = document.querySelectorAll('.section[data-id="homepage_slideshow"] .VueCarousel-slide .slideshow-section__background img')
+  let first_slide = document.querySelector('.section[data-id="homepage_slideshow"] .VueCarousel-inner .VueCarousel-slide:first-child')
+  if (!first_slide.classList.contains('VueCarousel-slide-active')) {
+    first_slide.classList.add('VueCarousel-slide-active')
+  }
+
   for (let i = 0; i < vue_slides.length; i++) {
     let the_image = vue_slides[i]
     let image_wrap = the_image.parentElement
+    let defer_slides
 
-    let new_image_1 = the_image.cloneNode(true)
-    let new_image_2 = the_image.cloneNode(true)
+    defer_slides = setInterval(() => {
+      if (the_image.getAttribute('lazy') == 'loaded') {
+        let new_image_1 = the_image.cloneNode(true)
+        let new_image_2 = the_image.cloneNode(true)
 
-    new_image_1.classList.add('sub_1')
-    new_image_2.classList.add('sub_2')
+        let new_wrap_0  = document.createElement('DIV')
+        let new_wrap_1  = document.createElement('DIV')
+        let new_wrap_2  = document.createElement('DIV')
 
-    image_wrap.appendChild(new_image_1)
-    image_wrap.appendChild(new_image_2)
-    console.log(new_image_1)
+        new_wrap_0.classList.add('sub_wrap-1')
+        new_wrap_1.classList.add('sub_wrap-2')
+        new_wrap_2.classList.add('sub_wrap-3')
+
+        new_wrap_0.appendChild(the_image)
+        new_wrap_1.appendChild(new_image_1)
+        new_wrap_2.appendChild(new_image_2)
+
+        image_wrap.appendChild(new_wrap_0)
+        image_wrap.appendChild(new_wrap_1)
+        image_wrap.appendChild(new_wrap_2)
+
+        console.log('lazy_loaded')
+        clearInterval(defer_slides)
+      }
+    }, 100);
   }
 }
 
-for (let i = 0; i < vue_slides.length; i++) {
-  let the_image = vue_slides[i]
-  let image_wrap = the_image.parentElement
-  let defer_slides
-
-  defer_slides = setInterval(() => {
-    if (the_image.getAttribute('lazy') == 'loaded') {
-      let new_image_1 = the_image.cloneNode(true)
-      let new_image_2 = the_image.cloneNode(true)
-
-      let new_wrap_0  = document.createElement('DIV')
-      let new_wrap_1  = document.createElement('DIV')
-      let new_wrap_2  = document.createElement('DIV')
-
-      new_wrap_0.classList.add('sub_wrap-1')
-      new_wrap_1.classList.add('sub_wrap-2')
-      new_wrap_2.classList.add('sub_wrap-3')
-
-      new_wrap_0.appendChild(the_image)
-      new_wrap_1.appendChild(new_image_1)
-      new_wrap_2.appendChild(new_image_2)
-
-      image_wrap.appendChild(new_wrap_0)
-      image_wrap.appendChild(new_wrap_1)
-      image_wrap.appendChild(new_wrap_2)
-
-      console.log('lazy_loaded')
-      clearInterval(defer_slides)
+const main_content_observer = (mutations) => {
+  console.log('Main content changed')
+  let home_slider = document.querySelector('.section[data-id="homepage_slideshow"]')
+  if (home_slider) {
+    if (!home_slider.classList.contains('mutated')) {
+      home_slider.classList.add('mutated')
+      init_slides()
     }
-  }, 100);
+  }
+  let sticky_bar = document.getElementById('sticky-bar')
+  if (sticky_bar) {
+    if (!main_content.classList.contains('stickies')) main_content.classList.add('stickies')
+  } else {
+    if (main_content.classList.contains('stickies')) main_content.classList.remove('stickies')
+  }
+}
+
+if (main_content) {
+
+  let main_observer = new MutationObserver(main_content_observer)
+  let options = {
+    childList: true,
+    subtree: true
+  }
+
+  main_observer.observe(main_content, options)
+  // console.log('Observing main content')
 }
